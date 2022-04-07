e86aea7a5204f57ee66e86f2626cb43953a124f4
Jonathan <Jonathan@iMac.local>
2022-04-07 15:35:11 -0400

    added glsl, vertex, and fragment shaders

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 import * as THREE from "https://unpkg.com/three@0.139.2/build/three.module.js";
+import vertexShader from "./shaders/vertex.glsl";
+import fragmentShader from "./shaders/fragment.glsl";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
@@ -7,14 +9,35 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   1000
 );
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({
+  antialias: true,
+});
+
 console.log(scene);
 console.log(camera);
 console.log(renderer);
 console.log("test");
 
 renderer.setSize(innerWidth, innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
 document.body.appendChild(renderer.domElement);
+
+const sphere = new THREE.Mesh(
+  new THREE.SphereGeometry(5, 50, 50),
+  new THREE.ShaderMaterial({
+    vertexShader,
+    fragmentShader,
+    uniforms: {
+      globeTexture: {
+        value: new THREE.TextureLoader().load("./img/globe.jpeg"),
+      },
+    },
+  })
+);
+
+scene.add(sphere);
+
+camera.position.z = 15;
 
 function animate() {
   requestAnimationFrame(animate);
---
+++ shaders/fragment.glsl
@@ -0,0 +1,7 @@
+uniform sampler2D globeTexture;
+
+varying vec2 vertexUV; 
+
+void main() {
+    gl_FragColor = vec4(texture2D(globeTexture, vertexUV).xyz, 1.0);
+}
