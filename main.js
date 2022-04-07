852e9486b51430f6c53ed11bf9049ba90b834f1a
Jonathan <Jonathan@iMac.local>
2022-04-07 16:21:28 -0400

    added mouse movement

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+import gsap from "gsap";
 import * as THREE from "https://unpkg.com/three@0.139.2/build/three.module.js";
 import vertexShader from "./shaders/vertex.glsl";
 import fragmentShader from "./shaders/fragment.glsl";
@@ -37,7 +38,7 @@ const sphere = new THREE.Mesh(
   })
 );
 
-scene.add(sphere);
+// scene.add(sphere);
 
 //creating atmosphere
 const atmosphere = new THREE.Mesh(
@@ -53,12 +54,43 @@ const atmosphere = new THREE.Mesh(
 atmosphere.scale.set(1.1, 1.1, 1.1);
 
 scene.add(atmosphere);
+
+const group = new THREE.Group();
+group.add(sphere);
+scene.add(group);
+
+const starGeometry = new THREE.BufferGeometry();
+
+const starMaterial = new THREE.PointsMaterial({
+  color: 0xffffff,
+});
+
+const stars
+
 camera.position.z = 15;
+
+const mouse = {
+  x: undefined,
+  y: undefined,
+};
 
 function animate() {
   requestAnimationFrame(animate);
   renderer.render(scene, camera);
-  sphere.rotation.y += 0.001;
+  sphere.rotation.y += 0.0015;
+  // group.rotation.y = mouse.x * 0.5;
+  gsap.to(group.rotation, {
+    x: -mouse.y * 0.5,
+    y: mouse.x * 0.5,
+    duration: 2,
+  });
 }
 
 animate();
+
+addEventListener("mousemove", () => {
+  mouse.x = (event.clientX / innerWidth) * 2 - 1;
+  mouse.y = -(event.clientY / innerHeight) * 2 + 1;
+});
+
+console.log(mouse);
